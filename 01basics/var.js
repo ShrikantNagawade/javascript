8d3bd5a0f081584a32b04de8e184152fd78d7ff4
ShrikantNagawade <89729774+ShrikantNagawade@users.noreply.github.com>
2023-10-18 08:18:46 +0000

    done

---
+++ 01basics/var.js
@@ -0,0 +1,13 @@
+//variables and data types
+const accountId=14444
+let accountEmail="shreenagawade555@gmail.com" //use this one most preffered 
+var acpassword="123" //not preffered 
+account_city="shrigonda"//we can but dont do this this is not best practice 
+//accountId=2 not allowed
+console.log(accountId)
+/*dont use var because of issue in block scope and functional scope */
+//(:)use it or not not neccesary
+//alert("hello") //we are using node not browse
+"use strict";//treat all js code as newer version
+console.table([accountEmail,acpassword])//giving output in tabular format. good practice
+ //mdn tc39 documentation 
